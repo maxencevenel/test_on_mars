9df2b5022e6a69fb6585f539c69c935afb57f7af
Maxence <maxence.venel@gmail.com>
2023-04-24 21:10:36 +0200

    feat: initializing project

---
+++ lib/main_development.dart
@@ -0,0 +1,7 @@
+import 'package:test_on_mars/app.dart';
+import 'package:test_on_mars/bootstrap.dart';
+
+
+void main() {
+  bootstrap(() => const App());
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:test_on_mars/app.dart';
+import 'package:test_on_mars/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
